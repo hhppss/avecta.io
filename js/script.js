5ebd53c4cbf563242d52d46e8595f7f628f77a24
hhppss <46415976+hhppss@users.noreply.github.com>
2020-02-08 23:11:53 +0300

    Добавление гамбургера на сайт, ожидание текста

--- js/script.js
+++ js/script.js
@@ -57,3 +57,13 @@ $(document).ready(function(){
     $('body,html').animate({scrollTop: top}, 800);
   });
 });
+$(document).ready(function() {
+  $('.header-burger').click(function(event) {
+    $('.header-burger, .header-menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  })
+  $('.header-link').click(function(event) {
+    $('.header-burger, .header-menu').removeClass('active');
+    $('body').removeClass('lock');
+  })
+});
